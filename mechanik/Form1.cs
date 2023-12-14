a03acdd2d3e0b820e662a7689374e301e361c440
Employee4272 <alekskozak12@gmail.com>
2023-12-14 14:53:29 +0100

    Add project files.

---
+++ mechanik/Form1.cs
@@ -0,0 +1,42 @@
+﻿namespace mechanik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (42 / 7);
+            number += 10;
+            number *= 3;
+            number = 71 / 3;
+            Console.WriteLine(number);
+
+            int count = 0;
+            count++;
+            count--;
+
+            string result = "cześć";
+            result += "i znowu wietnam";
+            output.Text = result;
+            result = "wartość wynosi: " + count;
+            result = "";
+
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
